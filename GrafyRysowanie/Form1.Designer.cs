000d6d4dea2a71e2f6c42e2e735cd7e6f530b8a3
D <D@ML0GSDRI>
2024-01-31 00:28:07 +0100

    Dodaj pliki projektów.

---
+++ GrafyRysowanie/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace GrafyRysowanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxAlgorithm = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBoxDirection = new System.Windows.Forms.CheckedListBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxAlgorithm
+            // 
+            this.checkedListBoxAlgorithm.FormattingEnabled = true;
+            this.checkedListBoxAlgorithm.Items.AddRange(new object[] {
+            "Deep first search",
+            "Breadth first search"});
+            this.checkedListBoxAlgorithm.Location = new System.Drawing.Point(12, 30);
+            this.checkedListBoxAlgorithm.Name = "checkedListBoxAlgorithm";
+            this.checkedListBoxAlgorithm.Size = new System.Drawing.Size(172, 61);
+            this.checkedListBoxAlgorithm.TabIndex = 1;
+            this.checkedListBoxAlgorithm.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBoxAlgorithm_ItemCheck);
+            // 
+            // checkedListBoxDirection
+            // 
+            this.checkedListBoxDirection.FormattingEnabled = true;
+            this.checkedListBoxDirection.Items.AddRange(new object[] {
+            "Undirected",
+            "Directed"});
+            this.checkedListBoxDirection.Location = new System.Drawing.Point(12, 115);
+            this.checkedListBoxDirection.Name = "checkedListBoxDirection";
+            this.checkedListBoxDirection.Size = new System.Drawing.Size(172, 61);
+            this.checkedListBoxDirection.TabIndex = 2;
+            this.checkedListBoxDirection.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBoxDirection_ItemCheck);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(12, 182);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(109, 182);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(244, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 418);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 211);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(172, 219);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Algorithm:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Direction:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 473);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.checkedListBoxDirection);
+            this.Controls.Add(this.checkedListBoxAlgorithm);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBoxAlgorithm;
+        private System.Windows.Forms.CheckedListBox checkedListBoxDirection;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GrafyRysowanie/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GrafyRysowanie
+{
+    public partial class Form1 : Form
+    {
+        private bool[] visited;
+        private int startVertex;
+        private int endVertex;
+        private Pen start;
+        private Pen end;
+
+        private const int r = 10;
+        private Graphics g;
+        private Pen pWierzcholek;
+        private Pen pWierzcholekAktywny;
+        private Pen pKrawedz;
+        private Wierzchlek MouseDownWierzcholek;
+
+        private List<Wierzchlek> wierzcholki = new List<Wierzchlek>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(500, 500);
+
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            pWierzcholek = new Pen(Color.Orange);
+            pWierzcholek.Width = 3;
+            pWierzcholekAktywny = new Pen(Color.Red);
+            pWierzcholekAktywny.Width = 3;
+
+            start = new Pen(Color.Green);
+            start.Width = 3;
+
+            end = new Pen(Color.Red);
+            end.Width = 3;
+
+            pKrawedz = new Pen(Color.Blue);
+            pKrawedz.Width = 10;
+            //pKrawedz.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            pKrawedz.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            checkedListBoxAlgorithm.SetItemChecked(0, true);
+            checkedListBoxDirection.SetItemChecked(0, true);
+        }
+
+
+
+
+        private void odrysujGraf()
+        {
+            g.Clear(Color.White);
+            foreach (Wierzchlek w in wierzcholki)
+            {
+                foreach (Wierzchlek wn in w.Nastpniki)
+                {
+                    g.DrawLine(pKrawedz, w.Polozenie, wn.Polozenie);
+                }
+            }
+
+            foreach (Wierzchlek w in wierzcholki)
+            {
+
+                if (w.Id == startVertex)
+                {
+                    g.DrawEllipse(start, w.Polozenie.X - r, w.Polozenie.Y - r, 2 * r, 2 * r);
+                }
+                else
+                {
+                    g.DrawEllipse(pWierzcholek, w.Polozenie.X - r, w.Polozenie.Y - r, 2 * r, 2 * r);
+
+                }
+                g.DrawString(w.Id.ToString(),
+                             new System.Drawing.Font("Microsoft Sans Serif", r),
+                             new SolidBrush(Color.Red),
+                             w.Polozenie.X + r,
+                             w.Polozenie.Y + r);
+                if (w == MouseDownWierzcholek)
+                {
+                    g.DrawEllipse(pWierzcholekAktywny, w.Polozenie.X - r, w.Polozenie.Y - r, 2 * r, 2 * r);
+                }
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void SetStartVertex(int vertexId)
+        {
+            startVertex = vertexId;
+            Array.Clear(visited, 0, visited.Length);
+            odrysujGraf();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && MouseDownWierzcholek != null)
+            {
+                odrysujGraf();
+                g.DrawLine(pKrawedz, MouseDownWierzcholek.Polozenie, e.Location);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && MouseDownWierzcholek != null)
+            {
+                foreach (Wierzchlek w in wierzcholki)
+                {
+                    if (w.Odleglosc(e.Location) < r)
+                    {
+                        if(checkedListBoxDirection.GetItemCheckState(1) == CheckState.Checked)
+                        {
+                            pKrawedz.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+                            MouseDownWierzcholek.Nastpniki.Add(w);
+                            
+
+                        }
+                        else
+                        {
+                            pKrawedz.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                            MouseDownWierzcholek.Nastpniki.Add(w);
+                            w.Nastpniki.Add(MouseDownWierzcholek);
+                        }
+                    }
+                }
+                MouseDownWierzcholek = null;
+                odrysujGraf();
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                wierzcholki.Add(new Wierzchlek(e.Location));
+                odrysujGraf();
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MouseDownWierzcholek = null;
+                foreach (Wierzchlek w in wierzcholki)
+                {
+                    if (w.Odleglosc(e.Location) < r)
+                    {
+                        MouseDownWierzcholek = w;
+                    }
+                }
+                odrysujGraf();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                visited = new bool[wierzcholki.Count + 1];
+                startVertex = -1;
+                endVertex = -1;
+
+                foreach (Wierzchlek w in wierzcholki)
+                {
+                    if (w.Odleglosc(e.Location) < r)
+                    {
+                        g.DrawEllipse(start, w.Polozenie.X - r, w.Polozenie.Y - r, 2 * r, 2 * r);
+
+                        SetStartVertex(w.Id);
+                    }
+                }
+            }
+        }
+        private void AppendToTextBox(string message)
+        {
+            textBox1.AppendText(message + Environment.NewLine);
+        }
+        private void checkedListBoxAlgorithm_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+            for (int i = 0; i < checkedListBoxAlgorithm.Items.Count; ++i)
+            {
+
+                if (i != e.Index)
+                {
+                    checkedListBoxAlgorithm.SetItemChecked(i, false);
+                }
+            }
+
+        }
+        private void DFS(int vertexId)
+        {
+            visited[vertexId - 1] = true;
+            //AppendToTextBox($"start: {vertexId}");
+            
+
+            foreach (Wierzchlek neighbor in wierzcholki[vertexId - 1].Nastpniki)
+            {
+                if (!visited[neighbor.Id - 1])
+                {
+                    AppendToTextBox($"z {vertexId} do {neighbor.Id}");
+                    DFS(neighbor.Id);
+                }
+            }
+
+        }
+        
+
+        private void BFS(int startVertex)
+        {
+            Queue<Wierzchlek> queue = new Queue<Wierzchlek>();
+            bool[] visited = new bool[wierzcholki.Count];
+
+            queue.Enqueue(wierzcholki[startVertex - 1]);
+            visited[startVertex - 1] = true;
+            int x = 1;
+            //AppendToTextBox($"level: {x}");
+            AppendToTextBox($"{x}){startVertex}");
+
+
+            while (queue.Count > 0)
+            {
+                //AppendToTextBox($"level {++x}");
+                Wierzchlek currentVertex = queue.Dequeue();
+
+                foreach (Wierzchlek neighbor in currentVertex.Nastpniki)
+                {
+
+                    if (!visited[neighbor.Id - 1])
+                    {
+
+                        queue.Enqueue(neighbor);
+                        visited[neighbor.Id - 1] = true;
+                        AppendToTextBox($"{++x}) visited: {neighbor.Id}");
+                    }
+                }
+
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            startVertex = -1;
+            wierzcholki.Clear();
+            Wierzchlek.ResetGraf();
+            textBox1.Clear();
+            odrysujGraf();
+        }
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Clear();
+            if (wierzcholki.Count < 2)
+            {
+                MessageBox.Show("Za mało wierzchołków");
+            }
+            else if (startVertex <= 0)
+            {
+                MessageBox.Show("Nie wybrano wierzchołka początkowego");
+            }
+            else if(checkedListBoxAlgorithm.GetItemCheckState(1) == CheckState.Checked)
+            {
+                AppendToTextBox($"Algorithm BFS.");
+                BFS(startVertex);
+                MessageBox.Show("Gotowe");
+            }
+            else
+            {
+                visited = new bool[wierzcholki.Count];
+                AppendToTextBox($"Algorithm DFS.");
+                DFS(startVertex);
+                MessageBox.Show("Gotowe");
+            }
+            
+        }
+
+        private void checkedListBoxDirection_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            for (int i = 0; i < checkedListBoxDirection.Items.Count; ++i)
+            {
+
+                if (i != e.Index)
+                {
+                    checkedListBoxDirection.SetItemChecked(i, false);
+                }
+            }
+        }
+    }
+}
